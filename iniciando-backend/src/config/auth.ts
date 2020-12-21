02a2d6f5653a2a9883e0c282fb05bebc55c2f7c9
Esdrasmelo <61204827+Esdrasmelo@users.noreply.github.com>
2020-12-20 23:27:35 -0300

    Changes
    Commit to save all changes

---
+++ iniciando-backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'd0bc88f59866b5a5bcf3fcfc3a46f0af',
+    expiresIn: '1d',
+  },
+};
---
+++ iniciando-backend/src/services/AuthenticateUserSession.ts
@@ -0,0 +1,45 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+import authConfig from '../config/auth';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserSession {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserSession;
